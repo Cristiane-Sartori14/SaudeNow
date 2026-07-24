76fa3312cd4c16a23b9c9d0ddcd6596414cd8041
Cristiane Sartori <sartori.cris@gmail.com>
2026-07-23 22:40:41 -0300

    chore: remove .history e .idea do repositório

--- .history/src/constants/Fonts_20260711212329.ts
+++
@@ -1,8 +0,0 @@
-const Fonts = {
-  title: 30,
-  subtitle: 22,
-  text: 18,
-  small: 16,
-};
-
-export default Fonts;
--- .history/src/constants/Spacing_20260711212338.ts
+++
@@ -1,9 +0,0 @@
-const Spacing = {
-  xs: 6,
-  sm: 10,
-  md: 16,
-  lg: 24,
-  xl: 32,
-};
-
-export default Spacing;
